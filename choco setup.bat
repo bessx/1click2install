5a3af5ce4c50eb6d86887c0ed37a231fc9ff48ed
2bon <RickXyCn@gmail.com>
2018-09-05 23:02:49 -0700

    choco

---
+++ choco setup.bat
@@ -0,0 +1,2 @@
+==========https://stackoverflow.com/questions/47861537/choco-command-not-recognized-when-run-as-administrator-on-windows?rq=1=====
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
