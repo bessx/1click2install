271c653b951663ddad68428b5c463b36d1ec47da
2bon <RickXyCn@gmail.com>
2018-09-06 23:03:25 -0700

    If you have Chocolatey installed, you can just run RefreshEnv to get updated environment variables into your current session

--- resetvars.bat
+++ resetvars.bat
@@ -1,3 +1,6 @@
+
+#If you have Chocolatey installed,
+#you can just run RefreshEnv to get updated environment variables into your current session
 ===========per https://stackoverflow.com/questions/171588/is-there-a-command-to-refresh-environment-variables-from-the-command-prompt-in-w==========
 @echo off
 %~dp0resetvars.vbs
