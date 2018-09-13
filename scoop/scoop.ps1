347d5cfd0de9be7b037777b0e9e9a9a789697bd9
2bon <RickXyCn@gmail.com>
2018-09-13 02:51:10 -0700

    $env:scoopApps = "C:\Users\" + $env:username + "\scoop\apps"

--- scoop/scoop.ps1
+++ scoop/scoop.ps1
@@ -1,6 +1,4 @@
 
 ======run 'powershell' as admin 2 use 'env'==https://stackoverflow.com/questions/34482278/setx-command-not-recognized-on-windows10/52133794#52133794===
-$env:scoop = "C:\Users\" + $env:username + "\scoop"
-$env:scoop
-$env:scoopApps = $env:scoop + "\apps"
+$env:scoopApps = "C:\Users\" + $env:username + "\scoop\apps"
 $env:scoopApps
