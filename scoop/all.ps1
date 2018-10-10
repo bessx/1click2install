ef957ae9c4561fc9ef7f32b0c4815b12aa87dc67
2bon <RickXyCn@gmail.com>
2018-10-10 04:03:22 -0700

    comment start with "#"

--- scoop/all.ps1
+++ scoop/all.ps1
@@ -1,15 +1,15 @@
 
-======run 'powershell' as admin 2 use 'env'==https://stackoverflow.com/questions/34482278/setx-command-not-recognized-on-windows10/52133794#52133794===
+#======run 'powershell' as admin 2 use 'env'==https://stackoverflow.com/questions/34482278/setx-command-not-recognized-on-windows10/52133794#52133794===
 $env:scoopApps = "C:\Users\" + $env:username + "\scoop\apps"
 $env:scoopApps
 
-scoop install gcc
-=======Phase 3: Create the gccPath environment variable===
-$env:gccPath = $env:scoopApps + "/gcc"
-$env:gccPath
-$env:gccBin = $env:gccPath + "/Bin"
-$env:gccBin
-$env:Path = $env:Path + ";" + $env:gccBin
+scoop install PowerShell
+#=======Phase 3: Create the PowerShellPath environment variable===
+$env:PowerShellPath = $env:scoopApps + "/PowerShell"
+$env:PowerShellPath
+$env:PowerShellBin = $env:PowerShellPath + "/Bin"
+$env:PowerShellBin
+$env:Path = $env:Path + ";" + $env:PowerShellBin
 $env:Path
 
-=======Phase 4: Test and ensure===
+#=======Phase 4: Test and ensure===
--- scoop/scoop.ps1
+++ scoop/scoop.ps1
@@ -1,4 +1,7 @@
 
-======run 'powershell' as admin 2 use 'env'==https://stackoverflow.com/questions/34482278/setx-command-not-recognized-on-windows10/52133794#52133794===
+#======run 'powershell' as admin 2 use 'env'==https://stackoverflow.com/questions/34482278/setx-command-not-recognized-on-windows10/52133794#52133794===
 $env:scoopApps = "C:\Users\" + $env:username + "\scoop\apps"
 $env:scoopApps
+
+
+#=========AppList: PowerShell,git,gcc,go,nodejs,python
