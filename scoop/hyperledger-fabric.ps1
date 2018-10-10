4178fcf27803d5eda4acc88c94b39dd6083d039b
2bon <RickXyCn@gmail.com>
2018-10-10 04:04:49 -0700

    hyperledger-fabric all in 1click

---
+++ scoop/hyperledger-fabric.ps1
@@ -0,0 +1,14 @@
+
+#======https://hyperledger-fabric.readthedocs.io/en/release-1.2/prereqs.html===
+#=========AppList: cURL,docker,go,nodejs,python,git
+npm install -g npm
+git config --global core.autocrlf false
+git config --global core.longpaths true
+git config --get core.autocrlf
+git config --get core.longpaths
+scoop update cURL
+npm install --global windows-build-tools
+npm install --global grpc
+
+
+#==========https://hyperledger-fabric.readthedocs.io/en/release-1.2/install.html=======
