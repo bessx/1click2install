c152d909294127a27d9605f25d70683f8816e21f
2bon <RickXyCn@gmail.com>
2018-11-20 17:00:25 -0800

    debug

--- brew/mac all.sh
+++ brew/mac all.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-=======http://railsapps.github.io/xcode-command-line-tools.html===========
+#=======http://railsapps.github.io/xcode-command-line-tools.html===========
 gcc
-xcode-select —install
+xcode-select --install
 xcode-select -p
-gcc —version
+gcc --version
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew doctor
 brew install git,
 // foxit reader,Spectacle,screencast-o-matic,Kubernetes
-brew cask install jetbrains-toolbox,intellij-idea,filezilla, mactex, wechat,google-drive-file-stream, google-backup-and-sync,flux,zoom,docker,
+brew cask install jetbrains-toolbox,intellij-idea,filezilla, mactex, wechat,google-drive-file-stream, google-backup-and-sync,flux,zoom,docker,virtualbox,
 
