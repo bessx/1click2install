2e6c2d8b80a3dcc359fe3775a3cc678287c7d3ff
2bon <RickXyCn@gmail.com>
2018-11-20 17:05:19 -0800

    brew install many, https://github.com/Homebrew/legacy-homebrew/issues/33624

--- brew/kubernetes.sh
+++ brew/kubernetes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #https://matthewpalmer.net/kubernetes-app-developer/articles/guide-install-kubernetes-mac.html
-brew cask install Docker,virtualbox
+brew cask install Docker virtualbox
 brew install kubectl
